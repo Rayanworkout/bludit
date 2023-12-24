83f998b374ce6651ca29aec834c7740d0489ae08
Rayan <rayan13170@protonmail.com>
2023-12-24 18:44:44 +0100

    added archive

---
+++ articles_backup/bl-content/databases/plugins/popeye/db.php
@@ -0,0 +1,2 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{"googleFonts":true,"darkMode":true,"dateFormat":"relative","showTags":true,"position":1}
---
+++ articles_backup/bl-content/databases/plugins/remote-content/db.php
@@ -0,0 +1,2 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{"webhook":"65883ebed7a80","source":"","position":1}
